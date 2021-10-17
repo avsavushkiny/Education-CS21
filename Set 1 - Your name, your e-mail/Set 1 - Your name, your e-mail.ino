94b1c1a2f20b2c9ce35ee8b4ee06d1125b11cd08
avsavushkiny <avsavushkiny@live.ru>
2021-10-17 16:40:14 +0300

    Create Set 1 - Your name, your e-mail.ino

---
+++ Set 1 - Your name, your e-mail/Set 1 - Your name, your e-mail.ino
@@ -0,0 +1,28 @@
+/*
+Week 0
+
+Set 1 - Your name, your e-mail
+
+Вывести свою фамилию и свой адрес электронной почты
+в монитор порта используя нижеприведенные конструкции
+
+\t - символ горизонтальной табуляции
+\n - символ новой строки
+\r - возврат каретки в начало строки
+Serial.print(F(“Hello World”)); - выводим текст из flash памяти
+
+doc A. Savushkin
+16.10.21
+*/
+
+void setup(void)
+{
+    //port speed
+    Serial.begin(9600);
+}
+
+void loop(void)
+{
+    //message
+    Serial.print("Savushkin \t e-mail: avsavushkiny@courseselectrical.onmicrosoft.com ");
+}
