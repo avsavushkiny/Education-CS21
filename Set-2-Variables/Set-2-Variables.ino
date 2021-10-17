14de06dc0287fb83c5f5dee5ffec89de8beb1655
avsavushkiny <avsavushkiny@live.ru>
2021-10-18 02:10:09 +0300

    Create Set-2-Variables.ino

---
+++ Set-2-Variables/Set-2-Variables.ino
@@ -0,0 +1,34 @@
+/*
+Week 1
+
+Set 2 - Variables
+
+Вывести значения из result
+
+a = 5;
+b = 2;
+a = a + 1;
+result = a - b;
+
+doc A. Savushkin
+18.10.21
+*/
+
+void setup(void)
+{
+    //port speed
+    Serial.begin(9600);
+}
+
+void loop(void)
+{
+    //data
+    int a = 5;
+    int b = 2;
+    a = a + 1;
+    int result = a - b;
+    
+    //output data
+    Serial.println(result);
+    Serial.println((String) "Result value = " + result);
+}
