9427abbfc1fc380bee6dbdb3ae0fa52cfab16403
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2022-01-25 17:52:12 +0300

    Update Set-gameDice-lcd1602.ino

--- Set-gameDice-lcd1602/Set-gameDice-lcd1602.ino
+++ Set-gameDice-lcd1602/Set-gameDice-lcd1602.ino
@@ -10,19 +10,30 @@ doc A. Savushkin
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+int8_t rndDice[3] = {0};
+
 void setup()
 {
-
+    lcd.begin(16, 2);
+    lcd.clear();
 }
 
 void loop()
 {
-
+    startGame();
 }
 
 void startGame()
 {
+    for (int a = 0; a <= 3; a++)
+    {
+        rndDice[a] = random(1, 7);
+        delay(150);
+    }
 
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print((String) rndDice[0] + " " + rndDice[1] + " " + rndDice[2]);
 }
 
 void startPage()
