830057baf9feb8a02a887b60ae369006f8550929
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2022-01-25 16:52:21 +0300

    Create Set-edu-dino.ino

---
+++ Set-edu-dino/Set-edu-dino.ino
@@ -0,0 +1,36 @@
+/*
+Week 000
+
+Set - Dice
+
+doc A. Savushkin
+25.01.22
+*/
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
+
+void startGame()
+{
+
+}
+
+void startPage()
+{
+
+}
+
+void overPage()
+{
+
+}
